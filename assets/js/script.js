122b1f9392ff9a899077206922f381d61f4ac69a
amirul <amirulhridoy1@gmail.com>
2021-01-14 23:57:50 +0600

    Hero and Services grid created

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+(function($){
+$(document).ready(function(){
+
+    //responsive navigation bar
+
+    $('.mobile-menu a').click(function(){
+        let menuStatus = $('nav.menu').css('display');
+
+        if(menuStatus == 'block'){
+            $('.mobile-menu a').html('<i class="fas fa-bars"></i>');
+        }else if(menuStatus == 'none'){
+            $('.mobile-menu a').html('<i class="fas fa-times"></i>')
+        }
+
+        $('nav.menu').slideToggle();
+    });
+
+
+    $(window).resize(function(){
+        let bodyWidth = $('body').width();
+
+        if(bodyWidth > 992){
+            $('nav.menu').show();
+            $('.mobile-menu a').html('<i class="fas fa-bars"></i>');
+        }else{
+            $('nav.menu').hide();
+        }
+    })
+
+
+});
+})(jQuery);
